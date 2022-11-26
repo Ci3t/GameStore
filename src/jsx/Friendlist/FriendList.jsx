584ccea923a439fa91ae0e34e116867a0cef5186
Ci3t <ranigfx@gmail.com>
2022-11-26 20:56:42 +0200

    Fetching Read Data & Friends + Delete

--- src/jsx/Friendlist/FriendList.jsx
+++ src/jsx/Friendlist/FriendList.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useEffect } from 'react'
 function FriendList() {
+const [friends,setFriends] = useState([])
+
+  useEffect(()=>{
+    const getFriends = async ()=>{
+
+      const {data} = await axios.get(`https://63738f8d0bb6b698b60f9519.mockapi.io/gamestore`)
+      
+      setFriends(data)
+    } 
+    getFriends()
+  },[])
+
+  const deleteFriends = async (id)=>{
+
+    await axios.delete(`https://63738f8d0bb6b698b60f9519.mockapi.io/gamestore/${id}`)
+
+    setFriends(
+      friends.filter((friend )=>{ return friend.id!== id})
+    )
+  }
+
   return (
-    <div>FriendList</div>
+    <div>
+      <ul>
+        {friends.map(friend=> {
+          return (
+            <React.Fragment  key={friend.id}>
+            <li >{friend.name}</li>
+            <li ><img src={friend.avatar} /></li>
+            <li><button onClick={()=>{
+              deleteFriends(friend.id)
+            }}>X</button></li>
+            </React.Fragment>
+          )
+        })}
+      </ul>
+    </div>
   )
 }
 
